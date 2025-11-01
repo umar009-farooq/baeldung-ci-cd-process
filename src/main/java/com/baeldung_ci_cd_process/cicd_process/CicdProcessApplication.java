2b64476b07bb60e4f22a39ee539851961e844de4
umar009 <rusumumarfarooq@gmail.com>
2025-11-01 18:38:12 +0530

    commit all

---
+++ src/main/java/com/baeldung_ci_cd_process/cicd_process/CicdProcessApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung_ci_cd_process.cicd_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baeldung_ci_cd_process/cicd_process/CicdProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung_ci_cd_process.cicd_process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
